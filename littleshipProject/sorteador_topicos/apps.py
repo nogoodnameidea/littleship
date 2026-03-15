4de5445c6eaadbbb7e3c5255e34fb3a3a726800f
David Samuel <samuelmarinhodasilvamarinho@gmail.com>
2026-03-15 13:35:31 +0100

    Movendo o repositório

---
+++ littleshipProject/sorteador_topicos/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SorteadorTopicosConfig(AppConfig):
+    name = 'sorteador_topicos'
---
+++ littleshipProject/sorteador_topicos/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def sorteador_topicos(request):
+    return HttpResponse('<button>Olha isso!</button>')
