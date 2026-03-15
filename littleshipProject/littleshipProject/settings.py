0bd44bc62d32be675bca53f7e3f5bb9973133bd6
David Samuel <samuelmarinhodasilvamarinho@gmail.com>
2026-03-15 18:43:59 +0100

    Tentativa número 5 de resolver o problema

--- littleshipProject/littleshipProject/settings.py
+++ littleshipProject/littleshipProject/settings.py
@@ -104,7 +104,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR /'static'
+STATIC_ROOT = BASE_DIR /'staticfiles'
 STATICFILES_DIRS = [
     'littleshipProject/static/'
 ]
